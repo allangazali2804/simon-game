ba7c1be00c65b7ced547da2d9a97312b7c4f21de
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-10-09 20:35:56 +0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,70 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+$(".btn").click(function() {
+
+    //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+    var userChosenColour = $(this).attr("id");
+  
+    //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    //$("#" + userChosenColour).fadeIn(150).fadeOut(150).fadeIn(150);
+    animatePress(userChosenColour);
+  
+    //console.log(userClickedPattern);
+  
+  });
+
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+
+//2. Create a new variable called level and start at level 0.
+var level = 0;
+
+//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+$(document).keypress(function() {
+  if (!started) {
+
+    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+// Function to make a new Sequence to the game
+function nextSequence(){
+      //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  level++;
+
+  //5. Inside nextSequence(), update the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level);
+
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn().fadeOut().fadeIn();
+    playSound(randomChosenColour);
+}
+
+// Callback Function to Play a Sound
+function playSound(name){
+    var audio = new Audio("sounds/" + name +".mp3");
+    audio.play();
+}
+
+// Callback Function to make an animation
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(() => {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+// Event Detection when a key is pressed
+
+$(document).keypress(nextSequence);
